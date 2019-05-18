365e7b3512ac45fe9906372aba7640a3acc25fd7
Administrator <Administrator@USERCHI-ISH06FH>
2019-05-18 23:30:45 +0800

    2019-5-18-hibernate

---
+++ src/com/hibernate/pojo/Book.java
@@ -0,0 +1,49 @@
+package com.hibernate.pojo;
+
+public class Book {
+
+	private Integer no;
+	private String name;
+	private String author;
+	private String publish;
+	private Double price;
+	private Integer store;
+	public Integer getNo() {
+		return no;
+	}
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublish() {
+		return publish;
+	}
+	public void setPublish(String publish) {
+		this.publish = publish;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Integer getStore() {
+		return store;
+	}
+	public void setStore(Integer store) {
+		this.store = store;
+	}
+	
+	
+}
---
+++ src/com/hibernate/service/impl/HibernateTest.java
@@ -0,0 +1,36 @@
+package com.hibernate.service.impl;
+
+import java.util.Date;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+
+import com.hibernate.pojo.Book;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		//获取SessionFactory对象
+		Session s = sf.openSession();
+		//通过SessionFactory对象获取session
+		s.beginTransaction();
+		//在session基础上开启一个事物
+		Book book= new Book();
+		book.setName("应召女郎");
+		book.setAuthor("大屌哥");
+		book.setPrice(19.8);
+		book.setPublish("2019-5-18");
+		book.setStore(50);
+		s.save(book);
+		//调用session的save方法把对象结果保存到数据库中
+		s.getTransaction().commit();
+		//提交事物
+		s.close();
+		sf.close();
+
+	}
+
+}
